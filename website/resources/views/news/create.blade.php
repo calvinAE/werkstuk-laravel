7296bf61cce55d3fb62a349bcfcf092346c53ee8
calvinAE <58550080+calvinAE@users.noreply.github.com>
2022-12-17 13:36:14 +0100

    Added news views

--- website/resources/views/news/create.blade.php
+++ website/resources/views/news/create.blade.php
@@ -1,1 +1,36 @@
+@extends('layouts.app')
 
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-6">
+                <form method="POST" action="{{ route('news.store') }}" enctype="multipart/form-data">
+                    @csrf
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    <div class="card text-left">
+                        <div class="card-body">
+                            <label for="title">Title</label>
+                            <input type="text" style="" class="form-control" name="title" id="title">
+                            <label for="content">Content</label>
+                            <textarea class="form-control" id="content" name="content" rows="3"></textarea>
+                            <label for="file">Upload news cover</label>
+                            <input type="file" name="cover_img" class="form-control-file" id="file">
+                            <button type="submit" class="btn btn-primary">
+                                Add News
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ website/resources/views/news/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+@section('content')
+    <div class="row justify-content-center">
+        <h1 style="text-align: center">News overview</h1>
+
+    </div>
+    <div class="row justify-content-center ">
+        <div class="col-10">
+            <button type="button" style="margin-bottom: 10px; float: right;"
+                onclick="location.href='{{ url('/news/create') }}'" class="btn btn-success float-right">New
+                Post</button>
+        </div>
+
+        @foreach ($news as $newsItem)
+            <div class="col-md-5">
+                <div class="card text-left  mx-auto d-flex"
+                    style="min-width:260px; margin-bottom:40px; max-width:500px; min-height:300px; box-shadow: rgba(0, 0, 0, 0.15) 0px 15px 25px, rgba(0, 0, 0, 0.05) 0px 5px 10px;">
+                    <a href="" class="card-body">
+
+                        <h1 class="card-title" style="font-size: 30px; font-weight:bold; ">{{ $newsItem->title }}</h1>
+                        <img src="{{ 'data:image/png;base64,' . base64_encode($newsItem->cover_img) }}" alt="cover image">
+                        <p class="card-text">{{ $newsItem->content }}</p>
+                        <small>{{ 'Author: ' . $newsItem->user->name }}
+                        </small><br>
+                        <small> {{ date('l, d M Y', strtotime($newsItem->publishing_date)) }}</small>
+
+                        <br>
+
+                    </a>
+
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endsection
