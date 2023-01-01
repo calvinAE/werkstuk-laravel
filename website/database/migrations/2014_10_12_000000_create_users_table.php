a8feb8d97e710562308bfef44794bb53609084a0
calvinAE <58550080+calvinAE@users.noreply.github.com>
2023-01-01 20:30:22 +0100

    Update role migration

--- website/database/migrations/2014_10_12_000000_create_users_table.php
+++ website/database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('isAdmin')->default(false);
+            $table->string('role')->default('user');
             $table->date('birthday');
             $table->string('biography')->nullable();
             $table->rememberToken();
