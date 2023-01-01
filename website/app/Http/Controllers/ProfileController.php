30250ff2be242dcdfa5899433f1ce100fb83e6fc
calvinAE <58550080+calvinAE@users.noreply.github.com>
2023-01-01 20:30:09 +0100

    Update ProfileController.php

--- website/app/Http/Controllers/ProfileController.php
+++ website/app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
@@ -30,6 +31,14 @@ class ProfileController extends Controller
      */
     public function update(ProfileUpdateRequest $request)
     {
+
+
+        $request->get('name');
+        $request->get('email');
+        $request->get('avatar');
+        $request->get('biography');
+
+
         $request->user()->fill($request->validated());
 
         if ($request->user()->isDirty('email')) {
