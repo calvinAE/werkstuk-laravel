4fa9c2e10bcaf688e0b50f1ab33faeb03578cd83
calvinAE <58550080+calvinAE@users.noreply.github.com>
2023-01-15 21:36:23 +0100

    Added About view

---
+++ website/resources/views/about.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+@section('content')
+    <div class="row justify-content-center">
+        <div class="col-5">
+<table class="table">
+  <thead>
+    <tr>
+      <th>Sources</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Github Repository</th>
+      <td><a href="https://github.com/calvinAE/werkstuk-laravel">Werkstuk-laravel</a></td>
+    </tr>
+    <tr>
+
+      <th scope="row">Laravel documentation</th>
+      <td> <a href="https://laravel.com/docs/9.x">https://laravel.com/docs/9.x/</a>
+    </tr>
+
+  </tbody>
+</table>
+        </div>
+</div>
+
+@endsection
