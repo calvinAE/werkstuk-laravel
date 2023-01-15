9e441ee5afccb5fbbdfb1f86f2cc59b640da63e5
calvinAE <58550080+calvinAE@users.noreply.github.com>
2023-01-15 22:25:56 +0100

    Updated routes

--- website/routes/web.php
+++ website/routes/web.php
@@ -22,6 +22,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/users', [UserController::class, 'index'])->name('users');
+Route::post('makeAdmin/{userId}', [UserController::class, 'makeAdmin'])->name('makeAdmin');
+Route::post('removeUser/{userId}', [UserController::class, 'removeUser'])->name('removeUser');
+Route::post('delete/{faqId}', [FAQController::class, 'delete'])->name('delete');
+
 
 Route::resource('news', NewsController::class);
 Route::resource('faq', FAQController::class);
